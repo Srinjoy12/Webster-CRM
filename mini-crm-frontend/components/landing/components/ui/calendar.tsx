6c8a70d3bda4e35eab12fba90e95a84f2a4a7f4e
Srinjoy12 <125884812+Srinjoy12@users.noreply.github.com>
2025-05-12 07:47:31 +0530

    Fix TypeScript error for DayPicker custom icons in Calendar

--- mini-crm-frontend/components/landing/components/ui/calendar.tsx
+++ mini-crm-frontend/components/landing/components/ui/calendar.tsx
@@ -53,9 +53,9 @@ function Calendar({
       }}
       components={{
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
